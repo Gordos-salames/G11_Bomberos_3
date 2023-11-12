2a28dcf33b110ba645e56f197fc1707dde557b10
M4kyy <gonzalocuk735@gmail.com>
2023-11-12 20:11:30 -0300

    vistas
    -

---
+++ src/g11_bomberos/vista/G11_BomberosVista.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package g11_bomberos.vista;
+
+/**
+ *
+ * @author SCP-062
+ */
+public class G11_BomberosVista {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
